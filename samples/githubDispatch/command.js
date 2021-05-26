830db19d7275233535c1b2c6a23270a9621afbe7
Matthew <nicetry.ihatemyself.xyz>
2021-05-25 19:05:41 -0700

    don't try if know it will fail

--- samples/githubDispatch/command.js
+++ samples/githubDispatch/command.js
@@ -1,4 +1,4 @@
-const { Octokit } = require('octokit')
+const { Octokit } = require("octokit");
 /**
 * This is the implementation of your Fusebot command.
 * You can run it from Slack with `/fusebot trigger-fusebit-pipeline ...`.
@@ -13,37 +13,50 @@ const { Octokit } = require('octokit')
 * @param ctx {FusebotContext}
 */
 module.exports = async (ctx) => {
-    if (ctx.body.args.length === 0 || ctx.body.args[0] === "help") {
-        return help(ctx)
+  if (ctx.body.args.length === 0 || ctx.body.args[0] === "help") {
+    return help(ctx);
+  }
+  if (!ctx.configuration.PAT) {
+    await ctx.client.send(
+      "unable to find github PAT in configuration, please set a PAT in configuration with the scope of * on repo and write on action."
+    );
+  }
+  const octokit = new Octokit({ auth: ctx.configuration.PAT });
+  const [userRepo, workflowId, branchTagName] = ctx.body.args;
+  let isError = false;
+  if (!userRepo) {
+    isError = true;
+    await ctx.client.send("Username/repository was not found.");
+  }
+  if (!workflowId) {
+    isError = true;
+    await ctx.client.send("WorkflowId/workflowFileName was not found.");
+  }
+  if (!branchTagName) {
+    isError = true;
+    await ctx.client.send("Branch name or tag name was not found.");
+  }
+  if (isError) {
+    return;
+  }
+  await octokit.request(
+    "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+    {
+      owner: userRepo.split("/")[0],
+      repo: userRepo.split("/")[1],
+      workflow_id: workflowId,
+      ref: branchTagName,
     }
-    if (!ctx.configuration.PAT) {
-        await ctx.client.send("unable to find github PAT in configuration, please set a PAT in configuration with the scope of * on repo and write on action.")
-    }
-    const octokit = new Octokit({ auth: ctx.configuration.PAT });
-    const [userRepo, workflowId, branchTagName] = ctx.body.args
-    if (!userRepo) {
-        await ctx.client.send("Username/repository was not found.")
-    }
-    if (!workflowId) {
-        await ctx.client.send("WorkflowId/workflowFileName was not found.")
-    }
-    if (!branchTagName) {
-        await ctx.client.send("Branch name or tag name was not found.")
-    }
-    await octokit.request('POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-        owner: userRepo.split("/")[0],
-        repo: userRepo.split("/")[1],
-        workflow_id: workflowId,
-        ref: branchTagName
-    })
-    await ctx.client.send(`workflow ${workflowId} on ${userRepo} triggered with branch/tag ${branchTagName}!`)
+  );
+  await ctx.client.send(
+    `workflow ${workflowId} on ${userRepo} triggered with branch/tag ${branchTagName}!`
+  );
 };
 
-const help = async(ctx) => {
-    await ctx.client.send(`
-    - /fusebot github-dispatch <username>/<reponame> <workflow id> or <workflow file name ie: publish.yml> <branch name> or <tag name> 
-    - /fusebot github-dispatch help - display this help
-    e.g.
-    /fusebot github-dispatch fusebit/fusebot publish.yaml master
-    `)
-}
+const help = async (ctx) => {
+  await ctx.client.send(`
+- /fusebot github-dispatch <username>/<reponame> <workflow id> or <workflow file name ie: publish.yml> <branch name> or <tag name> 
+- /fusebot github-dispatch help - display this help
+e.g.
+/fusebot github-dispatch fusebit/fusebot publish.yaml master`);
+};
